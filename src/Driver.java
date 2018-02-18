5c4be2c072742849d1d7c78a9c65ae4bd6cd7dc2
Salman Sowdagar <b00061479@aus.edu>
2018-02-18 15:01:32 +0400

    Feature B added

--- src/Driver.java
+++ src/Driver.java
@@ -1,8 +1,14 @@
 
+
 public class Driver {
 
+	public void featureb ()
+	{
+		System.out.println("its me B");
+	}
 	public static void main(String[] args) {
 		System.out.println("Hello World");
+		
 	}
 
 }
